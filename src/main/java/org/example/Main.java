178a208c27a51aaee18cfed71cdb22f889f27136
israellichotti <israel.lichotti@al.infnet.edu.br>
2025-06-05 22:12:53 -0300

    adicionando classes, propriedades e metodos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,23 @@
 package org.example;
 
 import javax.swing.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
 
 
 
         System.out.println("Bem vindo a BikesOnTheStorm!");
         System.out.println("Como posso ajudá-lo(a)?");
         System.out.println("**************************");
+        System.out.println("1 - Gerenciar Bicicletas");
+        System.out.println("2 - Procurar Bicicleta");
+        System.out.println("3 - Listar Bicicletas");
+
+
 
 
 
